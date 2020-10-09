eb9468f33dcd0675cd930f4984323cb796f462d7
dardamyr <dardamyr@gmail.com>
2020-10-09 17:06:19 +0200

    adding PAN model, controller, migration, routes

---
+++ database/migrations/2020_10_09_141157_create_p_a_n_s_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePANSTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('p_a_n_s', function (Blueprint $table) {
+            $table->id();
+            $table->varchar("PAN",25)->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            
+           $table->foreign('user_id')->reference('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('p_a_n_s');
+    }
+}
