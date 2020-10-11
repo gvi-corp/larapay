09feee0685d36f965d0e697bd2e7987157a1e012
dardamyr <dardamyr@gmail.com>
2020-10-11 12:06:40 +0200

    first commit for digitized cards feature

---
+++ app/Http/Controllers/DigitizedCardController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DigitizedCard;
+use Illuminate\Http\Request;
+
+class DigitizedCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DigitizedCard  $digitizedCard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DigitizedCard $digitizedCard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DigitizedCard  $digitizedCard
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DigitizedCard $digitizedCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DigitizedCard  $digitizedCard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DigitizedCard $digitizedCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DigitizedCard  $digitizedCard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DigitizedCard $digitizedCard)
+    {
+        //
+    }
+}
